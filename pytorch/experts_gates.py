6faed1cff174577b4afe4b9664442615dc505506
Yamuna Krishnamurthy <yamunak@gmail.com>
2021-01-05 15:16:13 +0100

    Added presoftmax expert layers for moe pre softmax model Added escort tranform layer

--- pytorch/experts_gates.py
+++ pytorch/experts_gates.py
@@ -8,9 +8,9 @@ import torch.optim as optim
 # ### Networks and callbacks
 
 #Expert network
-class expert_layers(nn.Module):
+class expert_layers_shallow(nn.Module):
     def __init__(self, output):
-        super(expert_layers, self).__init__()
+        super(expert_layers_shallow, self).__init__()
         self.model = nn.Sequential(
                     nn.Linear(2, 4),
                     nn.ReLU(),
@@ -21,9 +21,21 @@ class expert_layers(nn.Module):
     def forward(self, input):
         return self.model(input)
 
-class expert_layers_1(nn.Module):
+class expert_layers_shallow_presoftmax(nn.Module):
     def __init__(self, output):
-        super(expert_layers_1, self).__init__()
+        super(expert_layers_shallow_presoftmax, self).__init__()
+        self.model = nn.Sequential(
+                    nn.Linear(2, 4),
+                    nn.ReLU(),
+                    nn.Linear(4,output)
+                )        
+        
+    def forward(self, input):
+        return self.model(input)
+    
+class expert_layers_deep(nn.Module):
+    def __init__(self, output):
+        super(expert_layers_deep, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(2, 32),
             nn.ReLU(),
@@ -38,18 +50,63 @@ class expert_layers_1(nn.Module):
     def forward(self, input):
         return self.model(input)
 
+class expert_layers_deep_presoftmax(nn.Module):
+    def __init__(self, output):
+        super(expert_layers_deep_presoftmax, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 4),
+            nn.ReLU(),
+            nn.Linear(4,output)
+        )        
+        
+    def forward(self, input):
+        return self.model(input)
+    
+# Implements escort transform to replace softmax
+class escort(nn.Module):
+    def __init__(self):
+        super(escort, self).__init__()
+        
+    def forward(self, input):
+        batch_size = input.shape[0]
+        numerator = torch.square(input)
+        denominator = torch.reshape(torch.sum(numerator, dim=1),(batch_size,1))
+        output = torch.div(numerator, denominator)
+        return output
+    
+class expert_layers_deep_escort(nn.Module):
+    def __init__(self, output):
+        super(expert_layers_deep_escort, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 4),
+            nn.ReLU(),
+            nn.Linear(4,output),
+            escort()
+        )        
+        
+    def forward(self, input):
+        return self.model(input)
+    
 # create a set of experts
-def experts(expert_layers, num_experts, num_classes):
+def experts(expert_layers_type, num_experts, num_classes):
     models = []
     for i in range(num_experts):
-        models.append(expert_layers(num_classes))
+        models.append(expert_layers_type(num_classes))
     return nn.ModuleList(models)
 
 
 #Gate network (Similar to the expert layer)
-class gate_layers(nn.Module):
+class gate_layers_shallow(nn.Module):
     def __init__(self, num_experts):
-        super(gate_layers, self).__init__()
+        super(gate_layers_shallow, self).__init__()
         self.model = nn.Sequential(
                     nn.Linear(2, 4),
                     nn.ReLU(),
@@ -60,9 +117,9 @@ class gate_layers(nn.Module):
     def forward(self, input):
         return self.model(input)
 
-class gate_layers_1(nn.Module):
+class gate_layers_deep(nn.Module):
     def __init__(self, num_experts):
-        super(gate_layers_1, self).__init__()
+        super(gate_layers_deep, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(2, 32),
             nn.ReLU(),
@@ -70,6 +127,22 @@ class gate_layers_1(nn.Module):
             nn.ReLU(),
             nn.Linear(16,num_experts),
             nn.Softmax(dim=1)
+        )
+        
+    def forward(self, input):
+        return self.model(input)
+
+
+class gate_layers_deep_escort(nn.Module):
+    def __init__(self, num_experts):
+        super(gate_layers_deep_escort, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16,num_experts),
+            escort()
         )
         
     def forward(self, input):
