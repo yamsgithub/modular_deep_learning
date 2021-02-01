3459ef2288128660d23826740a74554241ce98f9
Yamuna Krishnamurthy <yamunak@gmail.com>
2021-02-01 12:53:49 +0000

    1. Added computation of entropy of gate 2. Added cross_entropy_loss computation as the nn.CrossEntropyLoss works with raw input and not softmax input 3. Added importance loss to load balance experts (from Jeff Dean paper)"

--- pytorch/moe_models.py
+++ pytorch/moe_models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 # The moe architecture that outputs an expected output of the experts
 # based on the gate probabilities
@@ -11,6 +12,32 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
     print('device', device)
+
+w_importance = 0.1
+    
+def cross_entropy_loss(p, targets, reduction='mean'):
+    eps=1e-7
+    logp = torch.log(p+eps)
+    if reduction == 'none':
+        criterion = nn.NLLLoss(reduction=reduction)
+    else:
+        criterion = nn.NLLLoss()
+    crossentropy_loss = criterion(logp, targets)
+    return crossentropy_loss
+
+def entropy(p):
+    logp = torch.log2(p)
+    with torch.no_grad():
+        logp = np.nan_to_num(logp, neginf=0)
+        entropy_val = (-1*torch.sum(p*logp,dim=1)).mean()
+    return entropy_val
+
+def loss_importance(p):
+    loss_importance = 0.0
+    if p.shape[1] > 1:
+        importance = torch.sum(p,dim=0)
+        loss_importance = w_importance * torch.square(torch.std(importance)/torch.mean(importance))
+    return loss_importance
 
 class moe_expectation_model(nn.Module):
     
@@ -40,16 +67,17 @@ class moe_expectation_model(nn.Module):
         
         # expected sum of expert outputs
         output = torch.sum(p*x, 1)
-        
+
         return output
     
-    def train(self, trainloader, testloader, optimizer, loss_criterion, accuracy, epochs):
+    def train(self, trainloader, testloader, optimizer, loss_criterion, loss_importance, accuracy, epochs):
         expert_models = self.experts
         gate_model = self.gate
         running_loss = 0.0
         train_running_accuracy = 0.0 
         test_running_accuracy = 0.0
-        history = {'loss':[], 'accuracy':[], 'val_accuracy':[]}
+        running_entropy = 0.0
+        history = {'loss':[], 'accuracy':[], 'val_accuracy':[], 'entropy':[]}
         for epoch in range(epochs):  # loop over the dataset multiple times
             i = 0
             for inputs, labels in trainloader:
@@ -60,7 +88,12 @@ class moe_expectation_model(nn.Module):
                 optimizer.zero_grad()
 
                 outputs = self(inputs)
+                gate_outputs = self.gate(inputs)
+
                 loss = loss_criterion(outputs, labels)
+                if loss_importance:
+                     loss += loss_importance(gate_outputs)
+
                 loss.backward()
 
                 optimizer.step()
@@ -69,6 +102,9 @@ class moe_expectation_model(nn.Module):
 
                 acc = accuracy(outputs, labels)
                 train_running_accuracy += acc
+                #computing entropy
+                gate_outputs = self.gate(inputs)
+                running_entropy += entropy(gate_outputs)
 
                 i+=1
             
@@ -84,10 +120,12 @@ class moe_expectation_model(nn.Module):
 
             running_loss = running_loss / i
             train_running_accuracy = train_running_accuracy.cpu().numpy() / i
-            
+            with torch.no_grad():
+                running_entropy = running_entropy.cpu().numpy() / i
             history['loss'].append(running_loss)
             history['accuracy'].append(train_running_accuracy)
             history['val_accuracy'].append(test_running_accuracy)
+            history['entropy'].append(running_entropy)
 
             print('epoch %d' % epoch,
                   'training loss %.2f' % running_loss,
@@ -97,6 +135,7 @@ class moe_expectation_model(nn.Module):
             running_loss = 0.0
             train_running_accuracy = 0.0
             test_running_accuracy = 0.0
+            running_entropy = 0.0
         return history
 
 
@@ -133,13 +172,14 @@ class moe_pre_softmax_expectation_model(nn.Module):
         
         return output
     
-    def train(self, trainloader, testloader, optimizer, loss_criterion, accuracy, epochs):
+    def train(self, trainloader, testloader, optimizer, loss_criterion, loss_importance, accuracy, epochs):
         expert_models = self.experts
         gate_model = self.gate
         running_loss = 0.0
         train_running_accuracy = 0.0
         test_running_accuracy = 0.0
-        history = {'loss':[], 'accuracy':[], 'val_accuracy':[]}        
+        running_entropy = 0.0
+        history = {'loss':[], 'accuracy':[], 'val_accuracy':[], 'entropy':[]}        
         for epoch in range(epochs):  # loop over the dataset multiple times
             i = 0
             for inputs, labels in trainloader:
@@ -150,7 +190,13 @@ class moe_pre_softmax_expectation_model(nn.Module):
                 optimizer.zero_grad()
 
                 outputs = self(inputs)
+                gate_outputs = self.gate(inputs)
+
                 loss = loss_criterion(outputs, labels)
+                if loss_importance:
+                     loss += loss_importance(gate_outputs)
+                    
+
                 loss.backward()
 
                 optimizer.step()
@@ -159,9 +205,15 @@ class moe_pre_softmax_expectation_model(nn.Module):
               
                 acc = accuracy(outputs, labels)
                 train_running_accuracy += acc
-
-                i+=1
                 
+                #computing entropy
+                gate_outputs = self.gate(inputs)
+                running_entropy += entropy(gate_outputs)
+                
+                i+=1
+
+
+
             with torch.no_grad():
                 acc = 0.0
                 j = 0
@@ -174,10 +226,14 @@ class moe_pre_softmax_expectation_model(nn.Module):
 
             running_loss = running_loss / i
             train_running_accuracy = train_running_accuracy.cpu().numpy() / i
+            with torch.no_grad():
+                running_entropy = running_entropy.cpu().numpy() / i
+
             history['loss'].append(running_loss)
             history['accuracy'].append(train_running_accuracy)
             history['val_accuracy'].append(test_running_accuracy)
-
+            history['entropy'].append(running_entropy)
+            
             print('epoch %d' % epoch,
                   'training loss %.2f' % running_loss,
                   ', training accuracy %.2f' % train_running_accuracy,
@@ -186,9 +242,21 @@ class moe_pre_softmax_expectation_model(nn.Module):
             running_loss = 0.0
             train_running_accuracy = 0.0
             test_running_accuracy = 0.0
+            running_entropy = 0.0
         return history
 
 def moe_stochastic_loss(expert_outputs, gate_output, target):
+    expert_loss = []
+    for i in range(expert_outputs.shape[0]):
+        crossentropy_loss = cross_entropy_loss(expert_outputs[i], target, reduction='none')
+        expert_loss.append(torch.exp(-0.5*crossentropy_loss))
+    expert_loss = torch.stack(expert_loss)
+    expert_loss.transpose_(0,1)
+    expected_loss = -1*torch.log(torch.sum(gate_output * expert_loss, 1))
+    loss = torch.mean(expected_loss)
+    return loss.to(device)   
+
+def moe_stochastic_loss_1(expert_outputs, gate_output, target):
     expert_loss = []
     criterion = nn.CrossEntropyLoss(reduction='none')
     for i in range(expert_outputs.shape[0]):
@@ -211,32 +279,37 @@ class moe_stochastic_model(nn.Module):
         self.gate = gate.to(device)
         
         
-    def forward(self,input):
-        batch_size = input.shape[0]
+    def forward(self,inputs):
+        batch_size = inputs.shape[0]
         if batch_size > 1:
             x = []
             for i, expert in enumerate(self.experts):
-                x.append(expert(input))
+                x.append(expert(inputs))
             x = torch.stack(x)
             x.transpose_(0,1)
-            
-            p = self.gate(input)
-            
-            sample = torch.multinomial(p, 1)   
+
+            try:
+                p = self.gate(inputs)
+                sample = torch.multinomial(p, 1)
+            except:
+                print('inputs', inputs.shape, torch.sum(inputs))
+                print('p',p[0:5])
+                raise
             
             output = torch.cat([x[i][sample[i]] for i in range(batch_size)])
         else:
-            output = self.experts[0](input)
+            output = self.experts[0](inputs)
 
         return output
     
-    def train(self, trainloader, testloader, optimizer, loss_criterion, accuracy, epochs):
+    def train(self, trainloader, testloader, optimizer, loss_criterion, loss_importance, accuracy, epochs):
         expert_models = self.experts
         gate_model = self.gate
         running_loss = 0.0
         train_running_accuracy = 0.0
         test_running_accuracy = 0.0
-        history = {'loss':[], 'accuracy':[], 'val_accuracy':[]}                
+        running_entropy = 0.0
+        history = {'loss':[], 'accuracy':[], 'val_accuracy':[], 'entropy':[]}                
         for epoch in range(epochs):  # loop over the dataset multiple times
             i = 0
             for inputs, labels in trainloader:
@@ -253,19 +326,31 @@ class moe_stochastic_model(nn.Module):
                 x = torch.stack(x)
                 p = gate_model(inputs)
 
-                loss = loss_criterion(x.to(device, non_blocking=True), p.to(device, non_blocking=True) , labels)
+                #computing entropy
+                running_entropy += entropy(p)
+
+                loss = loss = loss_criterion(x.to(device, non_blocking=True), p.to(device, non_blocking=True) , labels)
+                if loss_importance:
+                     loss += loss_importance(p)
+                    
                 loss.backward()
 
                 optimizer.step()
 
                 running_loss += loss.item()
 
-                outputs = self(inputs)
+                try:
+                    outputs = self(inputs)
+                except:
+                    for name, param in self.gate.named_parameters():
+                        if param.requires_grad:
+                            print(name, param.data)
+                    
                 acc = accuracy(outputs, labels)
                 train_running_accuracy += acc
 
                 i+=1
-
+                
             with torch.no_grad():
                 acc = 0.0
                 j = 0
@@ -278,11 +363,15 @@ class moe_stochastic_model(nn.Module):
 
             running_loss = running_loss / i
             train_running_accuracy = train_running_accuracy.cpu().numpy() / i
+
+            with torch.no_grad():
+                running_entropy = running_entropy.cpu().numpy() / i
             
             history['loss'].append(running_loss)
             history['accuracy'].append(train_running_accuracy)
             history['val_accuracy'].append(test_running_accuracy)
-
+            history['entropy'].append(running_entropy)
+            
             print('epoch %d' % epoch,
                   'training loss %.2f' % running_loss,
                   ', training accuracy %.2f' % train_running_accuracy,
@@ -290,6 +379,7 @@ class moe_stochastic_model(nn.Module):
             running_loss = 0.0
             train_running_accuracy = 0.0
             test_running_accuracy = 0.0
+            running_entropy = 0.0
         return history
 
 
