4b2370985157b8c43e049db8d3c4bae1009dac3a
Yamuna Krishnamurthy <yamunak@gmail.com>
2022-07-04 22:39:16 +0100

    added stochastic and argmax

--- moe_models/moe_no_gate_model.py
+++ moe_models/moe_no_gate_model.py
@@ -19,8 +19,18 @@ else:
 
 class moe_no_gate_model(moe_models_base):
 
-    def __init__(self, num_experts=5, num_classes=10, augment=0, attention_flag=0, hidden=None, experts=None, gate=None, task='classification'):
-        super( moe_no_gate_model,self).__init__(num_experts, num_classes, augment, attention_flag, hidden, experts, gate, task)
+    def __init__(self, output_type='argmax', num_experts=5, num_classes=10, experts=None, gate=None, task='classification'):
+        super( moe_no_gate_model,self).__init__(num_experts=num_experts, num_classes=num_classes, experts=experts, gate=gate, task=task)
+        self.argmax = False
+        self.stochastic = False
+        self.expectation = False
+
+        if output_type == 'argmax':
+            self.argmax = True
+        elif output_type == 'stochastic':
+            self.stochastic = True
+        elif output_type == 'expectation':
+            self.expectation = True
 
     def forward(self, inputs, T=1.0):
 
@@ -39,28 +49,34 @@ class moe_no_gate_model(moe_models_base):
         
         h = torch.vstack(h).transpose_(0,1).to(device)
 
-        p = F.softmin(h/T, dim=1)
+        # TESTING: using the scalar weights. 
+        p = F.softmin(h/T, dim=1).detach()
+        #print('gate', p.shape)
 
         self.gate_outputs = p
-        
-        try:
-            m  = Categorical(p)
-            self.samples = m.sample().reshape(len(p), 1).to(device)
-        except:
-            raise
 
-        output = y[torch.arange(0,batch_size).reshape(batch_size,1).to(device), self.samples, :].squeeze()
+        if self.expectation:
+            # reshape gate output so probabilities correspond 
+            # to each expert
+            p = p.reshape(p.shape[0],p.shape[1], 1)
 
-        # # reshape gate output so probabilities correspond 
-        # # to each expert
-        # p = p.reshape(p.shape[0],p.shape[1], 1)
-
-        # # repeat probabilities number of classes times so
-        # # dimensions correspond
-        # p = p.repeat(1,1,y.shape[2])
-
-        # # expected sum of expert outputs
-        # output = torch.sum(p*y, 1)
+            # repeat probabilities number of classes times so
+            # dimensions correspond
+            p = p.repeat(1,1,y.shape[2])
+            
+            # expected sum of expert outputs
+            output = torch.sum(p*y, 1)
+        else:
+            try:
+                if self.stochastic:
+                    m  = Categorical(p)
+                    self.samples = m.sample().reshape(len(p), 1).to(device)
+                elif self.argmax:
+                    self.samples = torch.argmax(p, dim=1).to(device)
+            except:
+                raise
+            
+            output = y[torch.arange(0,batch_size).reshape(batch_size,1).to(device), self.samples.reshape(batch_size,1).to(device), :].squeeze()
 
         return output
 
