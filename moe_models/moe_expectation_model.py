95b1626cbd34666f40c0b373d39de852324a03e8
Yamuna Krishnamurthy <yamuna@login1.bede.dur.ac.uk>
2022-10-19 01:21:34 +0100

    pass device to training

--- moe_models/moe_expectation_model.py
+++ moe_models/moe_expectation_model.py
@@ -7,19 +7,13 @@ from sklearn.metrics import confusion_matrix
 from helper import moe_models
 from moe_models.moe_models_base import moe_models_base
 
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    print('device', device)
-else:
-    device = torch.device("cpu")
-    print('device', device)
-
 # The moe architecture that outputs an expected output of the experts
 # based on the gate probabilities
 class moe_expectation_model(moe_models_base):
     
-    def __init__(self, num_experts=5, num_classes=10, augment=0, attention_flag=0, hidden=None, experts=None, gate=None, task='classification'):
-        super(moe_expectation_model,self).__init__(num_experts, num_classes, augment, attention_flag, hidden, experts, gate, task)
+    def __init__(self, num_experts=5, num_classes=10, augment=0, attention_flag=0, hidden=None, experts=None, gate=None, task='classification',device = torch.device("cpu")):
+        super(moe_expectation_model,self).__init__(num_experts, num_classes, augment, attention_flag, hidden, experts, gate, task, device)
+        self.device = device
         
     def forward(self,inputs, T=1.0):
 
@@ -33,7 +27,7 @@ class moe_expectation_model(moe_models_base):
                 h.append(hidden_output.view(1,-1,hidden_output.shape[1]))
 
                 
-        y = torch.vstack(y).transpose_(0,1).to(device)
+        y = torch.vstack(y).transpose_(0,1).to(self.device)
         
         self.expert_outputs = y
 
@@ -42,7 +36,7 @@ class moe_expectation_model(moe_models_base):
             input_aug = torch.cat((inputs, output_aug), dim=1)
             p = self.gate(input_aug)
         elif self.attention:
-            h = torch.vstack(h).transpose_(0,1).to(device)
+            h = torch.vstack(h).transpose_(0,1).to(self.device)
             h_gate = self.gate(inputs)
 
             attention = self.attn(h, h_gate)
