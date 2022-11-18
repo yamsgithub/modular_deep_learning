34bed0e22a840159995a0b4d31ff7fb753cedf4c
yamsgithub <yamunak@gmail.com>
2022-11-18 17:05:08 +0000

    scripts to run experitments on cluster

---
+++ aaai_2022/scripts/schedule_cifar100_importance_model.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Generic options:
+
+#SBATCH --account=bdrap03  # Run job under project <project>
+#SBATCH --time=1-00:00:00  # Run for a max of 2 day
+
+# Node resources:
+# (choose between 1-4 gpus per node)
+
+#SBATCH --partition=gpu    # Choose either "gpu" or "infer" node type        
+#SBATCH --nodes=5 --ntasks-per-node=1 # Resources from a single node
+#SBATCH --gres=gpu:4      # 4 GPUS per node (plus 100% of node CPU and RAM per GPU)
+
+# Run commands:
+
+# Place other commands here
+
+cd /nobackup/projects/bdrap03/yamuna/modular_deep_learning/
+
+export MNN_HOME=/nobackup/projects/bdrap03/yamuna/modular_deep_learning/
+echo "MNN_HOME  : $MNN_HOME"
+
+export PYTHONPATH=$MNN_HOME
+echo "PYTHONPATH: $PYTHONPATH"
+
+conda init bash
+conda deactivate
+conda activate mnn
+
+python aaai_2022/notebooks/cifar100_importance_model.py -i $SLURM_ARRAY_TASK_ID
+
+echo "end of job"
---
+++ aaai_2022/scripts/schedule_cifar100_with_attention.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Generic options:
+
+#SBATCH --account=bdrap03  # Run job under project <project>
+#SBATCH --time=05:00:00         # Run for a max of 1 hour
+
+# Node resources:
+# (choose between 1-4 gpus per node)
+
+#SBATCH --partition=gpu    # Choose either "gpu" or "infer" node type
+#SBATCH --nodes=1          # Resources from a single node
+#SBATCH --gres=gpu:4      # One GPU per node (plus 100% of node CPU and RAM per GPU)
+
+# Run commands:
+
+# Place other commands here
+
+cd /nobackup/projects/bdrap03/yamuna/modular_deep_learning/
+
+export MNN_HOME=/nobackup/projects/bdrap03/yamuna/modular_deep_learning/
+echo "MNN_HOME  : $MNN_HOME"
+
+export PYTHONPATH=$MNN_HOME
+echo "PYTHONPATH: $PYTHONPATH"
+
+conda init bash
+conda activate mnn
+python aaai_2022/src/cifar10/cifar10_with_attention_model.py 
+
+echo "end of job"
