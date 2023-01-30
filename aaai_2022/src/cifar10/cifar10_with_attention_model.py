309b0734de94489fa796853e243fda54a9e8055e
yamsgithub <yamunak@gmail.com>
2023-01-30 08:50:42 +0000

    added wideres

--- aaai_2022/src/cifar10/cifar10_with_attention_model.py
+++ aaai_2022/src/cifar10/cifar10_with_attention_model.py
@@ -1,6 +1,11 @@
 import argparse
 from cifar10_moe_with_attention_training import *
 from moe_with_attention_training import *
+from torchvision.models import resnet18
+
+sys.path.append('WideResNet-pytorch-master')
+from wideresnet import WideResNet
+
 
 expert_layers_types = {'expert_layers': expert_layers,
                       'expert_layers_conv_2': expert_layers_conv_2}
@@ -21,6 +26,9 @@ runs = 1
 w_importance_range=[0.0]
 w_sample_sim_same_range = [0.0]
 w_sample_sim_diff_range = [0.0]
+d = 'default_distance_funct'
+distance_funct = default_distance_funct
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', help='expert layers type')
@@ -32,6 +40,7 @@ parser.add_argument('-r', help='number of runs')
 parser.add_argument('-E', help='number of epochs')
 parser.add_argument('-M', help='number of experts')
 parser.add_argument('-H', help='Hidden layers')
+parser.add_argument('-D', help='sample distance function')
 parser.add_argument('-i', help='Importance factor')
 parser.add_argument('-ss', help='sample similarity factor')
 parser.add_argument('-sd', help='sample dissimilarity factor')
@@ -56,6 +65,8 @@ if not args['E'] is None:
     num_epochs = int(args['E'])
 if not args['H'] is None:
     hidden = int(args['H'])
+if not args['D'] is None:
+    d = args['D']
 if not args['i'] is None:
     w_importance_range = [float(args['i'])]
 if not args['ss'] is None:
@@ -76,7 +87,7 @@ print('Hidden layers:', hidden)
 print('importance factor:', w_importance_range[0])
 print('sample similarity factor:', w_sample_sim_same_range[0])
 print('sample dissimilarity factor:', w_sample_sim_diff_range[0])
-
+print('sample distance function name', d)
 
 num_classes = 10
 
@@ -86,12 +97,29 @@ model_path = os.path.join(working_path, '../models/cifar10')
 
 if not os.path.exists(model_path):
     os.mkdir(model_path)
-    
+
+if d == 'resnet_distance_funct':
+    model_state = torch.load(os.path.join(model_path, 'resnet.ckpt'))
+    model = resnet18(pretrained=True).to(device)
+    model.load_state_dict(model_state)
+    distance_funct = resnet_distance_funct(model).distance_funct
+elif d == 'wideres_distance_funct':
+    number_of_layers = 40
+    model = WideResNet(depth=number_of_layers, num_classes=10, widen_factor=4)
+    checkpoint = torch.load('WideResNet-pytorch-master/runs/WideResNet-28-10/cifar10.pth.tar')
+    model.load_state_dict(checkpoint['state_dict'])
+    model = model.to(device)
+    model.eval()
+    distance_funct = resnet_distance_funct(model).distance_funct
+
+print('sample distance function', distance_funct)
+
 train_with_attention(m, mt, k, cifar10_trainloader, cifar10_testloader, 
                      expert_layers=expert_layers_type, gate_layers=gate_layers_type,
                      w_importance_range=w_importance_range,
                      w_sample_sim_same_range=w_sample_sim_same_range, 
                      w_sample_sim_diff_range=w_sample_sim_diff_range,
+                     distance_funct = distance_funct,
                      runs=runs, temps=[[1.0]*num_epochs], hidden=hidden, num_classes=num_classes, 
                      total_experts=total_experts, num_epochs=num_epochs, model_path=model_path)
 
