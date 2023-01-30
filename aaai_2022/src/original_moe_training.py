4aa33be0be96106a0233e68ce2930448860a3863
yamsgithub <yamunak@gmail.com>
2023-01-30 08:52:50 +0000

    added distance func

--- aaai_2022/src/original_moe_training.py
+++ aaai_2022/src/original_moe_training.py
@@ -17,19 +17,18 @@ from torch.utils.data import TensorDataset, random_split
 import torchvision.transforms.functional as TF
 from torch.distributions.categorical import Categorical
 
+device = torch.device("cpu")
+
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
-    print('device', device)
-else:
-    device = torch.device("cpu")
-    print('device', device)
 
+print('device', device)
 
 # import MoE expectation model. All experiments for this dataset are done with the expectation model as it
 # provides the best performance
 from moe_models.moe_expectation_model import moe_expectation_model
 from moe_models.moe_top_k_model import moe_top_k_model
-from moe_models.moe_models_base import default_optimizer
+from moe_models.moe_models_base import default_optimizer, default_distance_funct, resnet_distance_funct
 from helper.moe_models import cross_entropy_loss, stochastic_loss
 from helper.visualise_results import *
 
@@ -54,7 +53,7 @@ def train_original_model(model, model_name, k=1, trainloader=None, testloader=No
                          expert_layers=None, gate_layers=None, 
                          runs=10, temps=[[1.0]*20],                
                          w_importance_range=[0.0], w_sample_sim_same_range=[0.0], 
-                         w_sample_sim_diff_range=[0.0],
+                         w_sample_sim_diff_range=[0.0], distance_funct = default_distance_funct, 
                          num_classes=10, total_experts=5, num_epochs=20, model_path=None):
 
     moe_model_types = {'moe_expectation_model':(moe_expectation_model, cross_entropy_loss().to(device)),
@@ -94,7 +93,7 @@ def train_original_model(model, model_name, k=1, trainloader=None, testloader=No
                     moe_model = val['model'](k, total_experts, num_classes,
                                              experts=expert_models, gate=gate_model, device=device).to(device)
                 else:
-                    moe_model = val['model'](total_experts, num_classes,
+                    moe_model = val['model'](num_experts=total_experts, num_classes=num_classes,
                                          experts=expert_models, gate=gate_model, device=device).to(device)
                 
                 optimizer_moe = optim.Adam(moe_model.parameters(), lr=0.001, amsgrad=False, weight_decay=1e-3)
@@ -102,9 +101,10 @@ def train_original_model(model, model_name, k=1, trainloader=None, testloader=No
                 optimizer = default_optimizer(optimizer_moe=optimizer_moe)
 
                
-                hist = moe_model.train(trainloader, testloader,  val['loss'], optimizer = optimizer,
+                hist = moe_model.train(trainloader=trainloader, testloader=testloader,  
+                                       loss_criterion=val['loss'], optimizer = optimizer,
                                        T = T, w_importance=w_importance, w_sample_sim_same = w_sample_sim_same, 
-                                       w_sample_sim_diff = w_sample_sim_diff, 
+                                       w_sample_sim_diff = w_sample_sim_diff, distance_funct = distance_funct, 
                                        accuracy=accuracy, epochs=num_epochs)
                 val['experts'][total_experts] = {'model':moe_model, 'history':hist}                
 
