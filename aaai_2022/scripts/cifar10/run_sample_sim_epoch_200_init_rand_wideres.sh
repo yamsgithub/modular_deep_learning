c68935039caf563c616ac968bd6a46d0922dd45e
Yamuna Krishnamurthy <yamunak@gmail.com>
2023-02-23 13:27:00 +0100

    added new scripts

--- aaai_2022/scripts/cifar10/run_sample_sim_epoch_200_init_rand_wideres.sh
+++ aaai_2022/scripts/cifar10/run_sample_sim_epoch_200_init_rand_wideres.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-for i in 1e-6 1e-5 1e-4 1e-3; 
-    do for j in 1e-7 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1; 
+#!/bin/bash 
+# 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1
+for i in 1e-5 1e-4 1e-3; 
+    do for j in 1e-7; 
         do 
-        sbatch aaai_2022/scripts/cifar10/schedule_cifar10_original_model.sh -m cifar10_rand_init_wideres -ss $i -sd $j -D wideres_distance_funct -r 10 -M 10 -E 200; 
+         sbatch aaai_2022/scripts/cifar10/schedule_cifar10_original_model.sh -m cifar10_rand_init_wideres -ss $i -sd $j -D wideres_distance_funct -r 10 -M 10 -E 200; 
         
-        sbatch aaai_2022/scripts/cifar10/schedule_cifar10_with_attention.sh -m cifar10_with_attn_rand_init_wideres -ss $i -sd $j -D wideres_distance_funct -r 10 -M 10 -E 200; 
+        #sbatch aaai_2022/scripts/cifar10/schedule_cifar10_with_attention.sh -m cifar10_with_attn_rand_init_wideres -ss $i -sd $j -D wideres_distance_funct -r 10 -M 10 -E 200; 
         
-        sbatch aaai_2022/scripts/cifar10/schedule_cifar10_original_model.sh -m cifar10_rand_init_top_2_wideres -g gate_layers_top_k -k 2 -mt moe_top_k_model -ss $i -sd $j  -D wideres_distance_funct -r 10 -M 10 -E 200;
+         sbatch aaai_2022/scripts/cifar10/schedule_cifar10_original_model.sh -m cifar10_rand_init_top_2_wideres -g gate_layers_top_k -k 2 -mt moe_top_k_model -ss $i -sd $j  -D wideres_distance_funct -r 10 -M 10 -E 200;
         
-        sbatch aaai_2022/scripts/cifar10/schedule_cifar10_with_attention.sh -m cifar10_with_attn_rand_init_top_2_wideres -k 2 -mt moe_top_k_model -ss $i -sd $j  -D wideres_distance_funct -r 10 -M 10 -E 200;
+        #sbatch aaai_2022/scripts/cifar10/schedule_cifar10_with_attention.sh -m cifar10_with_attn_rand_init_top_2_wideres -k 2 -mt moe_top_k_model -ss $i -sd $j  -D wideres_distance_funct -r 10 -M 10 -E 200;
     done; 
 done
--- aaai_2022/scripts/cifar10/schedule_cifar10_train_no_gate_model.sh
+++ aaai_2022/scripts/cifar10/schedule_cifar10_train_no_gate_model.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Generic options:
-
-#SBATCH --account=bdrap03  # Run job under project <project>
+#SBATCH -J CIFAR10_no_gate
+#SBATCH -N 1
+#SBATCH --account=p71921  # Run job under project <project>
 #SBATCH --time=48:00:00  # Run for a max of 2 day
 
 # Node resources:
 # (choose between 1-4 gpus per node)
 
-#SBATCH --partition=gpu    # Choose either "gpu" or "infer" node type        
-#SBATCH --nodes=1 --ntasks-per-node=1 # Resources from a single node
+#SBATCH --partition=zen3_0512_a100x2   # Choose either "gpu" or "infer" node type        
+#SBATCH --qos zen3_0512_a100x2 
 #SBATCH --gres=gpu:1      # 4 GPUS per node (plus 100% of node CPU and RAM per GPU)
 
 # Run commands:
 
 # Place other commands here
 
-cd /nobackup/projects/bdrap03/yamuna/modular_deep_learning/
+cd /home/fs71921/yamunak/modular_deep_learning
 
-export MNN_HOME=/nobackup/projects/bdrap03/yamuna/modular_deep_learning/:/nobackup/projects/bdrap03/yamuna/modular_deep_learning/aaai_2022/src
+export MNN_HOME=/home/fs71921/yamunak/modular_deep_learning:/home/fs71921/yamunak/modular_deep_learning/aaai_2022/src
 echo "MNN_HOME  : $MNN_HOME"
 
 export PYTHONPATH=$MNN_HOME
