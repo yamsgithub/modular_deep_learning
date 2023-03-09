da948bbe841855c966524a52be31ff345826473d
yamsgithub <yamunak@gmail.com>
2023-03-09 09:41:03 +0000

    updates scripts to run single cifar100 model

--- aaai_2022/src/cifar100/cifar100_single_model.py
+++ aaai_2022/src/cifar100/cifar100_single_model.py
@@ -1,9 +1,19 @@
 from cifar100_original_moe_training import *
+from single_model_training import *
 
 model = 'cifar100_single_model'
 
-num_epochs = 40
+num_classes = 10
 
-runs = 5
+num_epochs = 1
 
-train_single_model(model, cifar100_trainloader, cifar100_testloader, num_classes, num_epochs, runs)
+runs = 10
+
+# Paths to where the trained models, figures and results will be stored. You can change this as you see fit.
+working_path = '/gpfs/data/p72053/yamuna_k'
+model_path = os.path.join(working_path, 'models/cifar100')
+
+if not os.path.exists(model_path):
+    os.mkdir(model_path)
+
+train_single_model(model, single_model, cifar100_trainloader, cifar100_valloader, num_classes, num_epochs, runs, model_path)
